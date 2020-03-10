1fdd388598090acfeb3d225e4860b30c64fce400
MAZTRO <design.mallet@gmail.com>
2020-03-10 17:47:23 +0000

    Add task #0 create a node

--- binary_trees.h
+++ binary_trees.h
@@ -1,4 +1,11 @@
+#ifndef BINARY_H
+#define BINARY_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
 /*Data structures */
+
 /* Basic Binary Tree */
 /**
  * struct binary_tree_s - Binary tree node
@@ -27,4 +34,7 @@ typedef struct binary_tree_s heap_t;
 
 /* Prototypes */
 /* Task 0 */
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+#endif
