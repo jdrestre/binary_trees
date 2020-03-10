75b289743428423eeb2f3f2573c3822c27ae7e76
MAZTRO <design.mallet@gmail.com>
2020-03-10 22:18:29 +0000

    Add task #3

--- binary_trees.h
+++ binary_trees.h
@@ -42,4 +42,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 /* Task 2 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
+/* Task 3 */
+void binary_tree_delete(binary_tree_t *tree);
+
 #endif
