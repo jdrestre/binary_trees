34918e4d35de030e5cd20b1c61f0cad0ec74ce1e
MAZTRO <design.mallet@gmail.com>
2020-03-11 17:59:10 +0000

    Fix out of stack at task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -23,25 +23,4 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		{return (count + 1); }
 	}
 	return (0);
-
-
- 	/* size_t count_1 = 0, count_2 = 0, count_3 = 0;
-
-	if (tree != NULL)
-	{
-		if (tree == NULL)
-		{return (0); }
-
-		count_1 = tree->left ? binary_tree_nodes(tree->left) : 0;
-		count_2 = tree->right ? binary_tree_nodes(tree->right) : 0;
-
-		if (tree->right || tree->right)
-		{
-			count_3 = count_1 + count_2 + 1;
-		}
-
-		return (count_3);
-
-	}
-	return (0); */
 }
