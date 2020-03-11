8aff6252536a9772cdf6f76e7a2647251655cc7f
MAZTRO <design.mallet@gmail.com>
2020-03-11 17:30:36 +0000

    Finish the task #13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,18 +8,23 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	size_t count_1 = 0, count_2 = 0, count_3 = 0;
 
 	if (tree != NULL)
 	{
 		if (tree == NULL)
 		{return (0); }
 
-		if (!tree->left && !tree->right)
-		{return (1); }
-		else
+		count_1 = tree->left ? binary_tree_nodes(tree->left) : 0;
+		count_2 = tree->right ? binary_tree_nodes(tree->right) : 0;
+
+		if (tree->right || tree->right)
 		{
-			return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+			count_3 = count_1 + count_2 + 1;
 		}
+
+		return (count_3);
+
 	}
 	return (0);
 }
